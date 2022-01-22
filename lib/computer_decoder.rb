b94af8d43ed7dc34ec8fbb8adc7237e3e8d9bbee
Logravia <j.alunans@gmail.com>
2022-01-22 20:45:57 +0200

    Partial implementation of computer decoder algorithm

--- lib/computer_decoder.rb
+++ lib/computer_decoder.rb
@@ -1,31 +1,76 @@
 require_relative 'process_code'
+require 'pry-byebug'
+
 class ComputerDecoder
   NUM_FOR_COLOR = { 1 => 'RED', 2 => 'GREEN', 3 => 'BLUE', 4 => 'YELLOW', 5 => 'CYAN', 6 => 'PURPLE' }.freeze
-  public
   extend ProcessCode
   def initialize
-    @unguessed_guesses = [*1111..6666]
-    @possible_guesses = [*1111..6666]
-    @first_guess = 1122
-    @cur_guess = nil
+    @unguessed_guesses = [*1111..6666].map! { |num| split_num_into_arr(num) }
+    @possible_guesses = [*1111..6666].map! { |num| split_num_into_arr(num) }
+    @cur_guess = [1,1,2,2]
     @guess_result = {perfect: nil, partial: nil}
   end
 
   def save_guess_result(result)
     @guess_result = result
+    rm_impossible_guesses
+    rm_guessed_guess
+    find_next_guess
   end
 
-  def guess
-    if unguessed_guesses == possible_guesses
-      int_to_colorcode(first_guess)
-    else
-      int_to_colorcode(cur_guess)
+
+  def find_next_guess
+    guess_scores = {}
+    unguessed_guesses.each do |unguessed|
+      guess_scores[unguessed] = {}
+      possible_guesses.each do |possible|
+        match_result = get_match_result_arr(unguessed, possible)
+        if guess_scores[unguessed].key? match_result
+          guess_scores[unguessed][match_result] += 1
+        else
+          guess_scores[unguessed][match_result] = 1
+        end
+      end
+    end
+    binding.pry
+    guess_scores
+  end
+
+  def get_match_result(code_one, code_two)
+    {perfect: ProcessCode.matches(code_one, code_two),
+      partial: ProcessCode.partial_matches(code_one, code_two)}
+  end
+
+  def get_match_result_arr(code_one, code_two)
+    [ProcessCode.matches(code_one, code_two), ProcessCode.partial_matches(code_one, code_two)]
+  end
+
+  # We remove all the guesses from possible guesses
+  # that do not match with the OUR GIVEN GUESS
+  # in the same way the SECRET CODE we are trying to break matched
+  def rm_impossible_guesses
+    possible_guesses.reject! do |guess|
+      match_result = get_match_result(guess, cur_guess)
+      guess_result == match_result
     end
   end
 
+  def rm_guessed_guess
+    delete_this = unguessed_guesses.find_index(cur_guess)
+    unguessed_guesses.delete_at(delete_this)
+  end
+
+  def guess
+      to_colorcode(cur_guess)
+  end
+
+  def split_num_into_arr(num)
+    num.to_s.chars.map! { |char| char.to_i }
+  end
+
   # Guesses are stored as an array of Strings not numbers for readability and convinience
-  def int_to_colorcode(code)
-    code.to_s.chars.map! {|char| NUM_FOR_COLOR[char.to_i]}
+  def to_colorcode(code)
+    code.map {|num| NUM_FOR_COLOR[num]}
   end
 
   private
--- lib/process_code.rb
+++ lib/process_code.rb
@@ -12,7 +12,7 @@ module ProcessCode
   # All perfect matches can also be partial matches
   # Thus muddying waters for an algorithm trying to find only partial matches
   # Returns modified array of the two given arrays
-  # Each element that was the same and at the same location in both arrays
+  # Each element with the same value and index
   # Is replaced with nil, in other words perfect matches are removed
   def self.no_perfect_matches(guess, code)
     guess = guess.dup
@@ -26,6 +26,7 @@ module ProcessCode
     [guess, code]
   end
 
+  # Partial
   def self.partial_matches(guess, code)
     partial_match_count = 0
     guess, code = no_perfect_matches(guess, code)
@@ -33,6 +34,10 @@ module ProcessCode
       next unless code.include?(color) && !color.nil?
 
       index_to_remove = code.find_index color
+      # When a partial match is found the pairs are removed,
+      # so that you couldn't have a partial match again with that color
+      # One pair is "removed" by incrementing to next element
+      # The other by declaring it nil
       code[index_to_remove] = nil
       partial_match_count += 1
     end
